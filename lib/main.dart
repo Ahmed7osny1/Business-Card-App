2c712c24920c071a81a073a9828ac7f08688529f
Ahmed Hosny <ahmedhosny6688@gmail.com>
2023-10-26 23:11:24 +0200

    Business Card App Practice

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BusinessCardApp());
+}
+
+class BusinessCardApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+
+}
