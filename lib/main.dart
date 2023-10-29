b249f0403cb1d84cc4f107c6b81c88d9260075dd
Ahmed Hosny <ahmedhosny6688@gmail.com>
2023-10-29 17:09:24 +0200

    Add Picture

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,31 @@ class BusinessCardApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF2B475E),
+        body: Column(
+          children: [
+            CircleAvatar(
+                radius: 92,
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 90,
+                  backgroundImage: AssetImage('images/myimage.jpg'),
+                )
+            )
+            /*ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(999)),
+              child: Image(
+                image: AssetImage('images/myimage.jpg'),
+                width: 300,
+                height: 200,
+              ),
+            )*/
+          ],
+        ),
+      ),
+    );
   }
 
 }
